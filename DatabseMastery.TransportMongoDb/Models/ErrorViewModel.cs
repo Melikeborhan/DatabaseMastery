2069379d664932211658baeadfc266107bf21ef8
Melike <22410051025@ogr.erbakan.edu.tr>
2026-04-02 09:28:38 +0300

    Add project files.

---
+++ DatabseMastery.TransportMongoDb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DatabseMastery.TransportMongoDb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
