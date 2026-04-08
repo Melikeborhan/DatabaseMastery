c57cc1dd747aa3601e518f562125c50d44e43dae
Melike <22410051025@ogr.erbakan.edu.tr>
2026-04-08 15:22:14 +0300

    fixed

---
+++ DatabseMastery.TransportMongoDb/Views/ViewComponents/DefaultComponents/_DefaultContactComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatabseMastery.TransportMongoDb.Views.ViewComponents.DefaultComponents
+{
+    public class _DefaultContactComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
