8dd2385c6b33d0be774296c1416982e4dab6ea1c
Melike <22410051025@ogr.erbakan.edu.tr>
2026-04-10 13:07:43 +0300

    -ViewComponent Dosya Dizini değiştirildi -AdminLayout Partial oluşturuldu

---
+++ DatabseMastery.TransportMongoDb/ViewComponents/AdminComponents/_AdminLayoutHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatabseMastery.TransportMongoDb.ViewComponents.AdminComponents
+{
+    public class _AdminLayoutHeadComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ DatabseMastery.TransportMongoDb/ViewComponents/AdminComponents/_AdminLayoutSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatabseMastery.TransportMongoDb.ViewComponents.AdminComponents
+{
+    public class _AdminLayoutSidebarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
