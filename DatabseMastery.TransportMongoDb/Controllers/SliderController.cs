298025b378bfe3aeed62c6c263486fb14ef91afa
Melike <22410051025@ogr.erbakan.edu.tr>
2026-04-10 15:27:26 +0300

    Slider için SliderController eklendi, Index yerine SliderList action oluşturuldu. SliderList view oluşturularak AdminLayout ile bağlandı ve içerik RenderBody üzerinden yönetildi. Script alanları partial view olarak ayrıldı.

---
+++ DatabseMastery.TransportMongoDb/Controllers/SliderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatabseMastery.TransportMongoDb.Controllers
+{
+    public class SliderController : Controller
+    {
+        public IActionResult SliderList()
+        {
+            return View();
+        }
+    }
+}
