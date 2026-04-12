31be5f814ebc91fa43ed80848edb2228218f1348
Melike <22410051025@ogr.erbakan.edu.tr>
2026-04-12 15:00:40 +0300

    -Slider servisi için altyapı çalışmaları tamamlandı
    -appsettings.json üzerinde MongoDB bağlantı ayarları yapıldı.
    
    -Slider işlemleri için ISliderService arayüzü (interface) tanımlandı.
    
    -Veri tabanı ayarlarını karşılayacak DatabaseSettings sınıfı eklendi.

---
+++ DatabseMastery.TransportMongoDb/Services/SliderServices/ISliderService.cs
@@ -0,0 +1,13 @@
+﻿using DatabseMastery.TransportMongoDb.Dtos.SliderDtos;
+
+namespace DatabseMastery.TransportMongoDb.Services.SliderServices
+{
+    public interface ISliderService
+    {
+        Task<List<ResultSliderDto>> GetSlidersAsync();
+        Task CreateSliderAsync(CreateSliderDto createSliderDto);
+        Task UpdateSliderAsync(UpdateSliderDto updateSliderDto);
+        Task<GetSliderByIdDto>GetSliderByIdAsync(string id);
+        Task DeleteSliderAsync(string id);
+    }
+}
---
+++ DatabseMastery.TransportMongoDb/Settings/DatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace DatabseMastery.TransportMongoDb.Settings
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string SliderCollectionName { get; set; }
+
+
+    }
+}
