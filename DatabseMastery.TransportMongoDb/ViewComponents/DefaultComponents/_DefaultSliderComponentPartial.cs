ec95bda9c4b27a4ab126486bc0588125afef0928
Melike <22410051025@ogr.erbakan.edu.tr>
2026-04-16 16:19:13 +0300

    fix: ViewComponent cagrisindaki bosluk hatasi ve eksik parantez duzeltildi

--- DatabseMastery.TransportMongoDb/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
+++ DatabseMastery.TransportMongoDb/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using DatabseMastery.TransportMongoDb.Services.SliderServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DatabseMastery.TransportMongoDb.Views.ViewComponents.DefaultComponents
 {
     public class _DefaultSliderComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public readonly ISliderService _sliderService;
+
+        //Constructor injection yaparak ISliderService'ı kullanabilir hale getiriyoruz. Bu sayede SliderController, ISliderService'ın implementasyonunu kullanarak slider işlemlerini gerçekleştirebilir.
+        //ISliderService enjekte ediliyor (Dependency Injection)
+        //Controller artık servise erişebiliyor
+        public _DefaultSliderComponentPartial(ISliderService sliderService)
         {
-            return View();
+            _sliderService = sliderService;
         }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _sliderService.GetAllSlidersAsync();
+            return View(values);
+
+        }
+
     }
 }
+
