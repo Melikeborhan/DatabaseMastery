e96849990c7087b5efc025e1f614dad882ff687b
Melike <22410051025@ogr.erbakan.edu.tr>
2026-04-17 08:43:25 +0300

    Brand için Entities ve Dtos oluşturuldu.

---
+++ DatabseMastery.TransportMongoDb/Dtos/BrandDtos/CreateBrandDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DatabseMastery.TransportMongoDb.Dtos.BrandDtos
+{
+    public class CreateBrandDto
+    {
+        public string BrandName { get; set; }
+        public string ImageUrl { get; set; }
+        public bool IsStatus { get; set; }
+    }
+}
