83ab0ee3e0a4effe3b3eeb9a5a69fce0d966f065
Melike <22410051025@ogr.erbakan.edu.tr>
2026-04-17 14:42:53 +0300

    offer Bölümünün tamamlanması

--- DatabseMastery.TransportMongoDb/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
+++ DatabseMastery.TransportMongoDb/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DatabseMastery.TransportMongoDb.Services.OfferServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DatabseMastery.TransportMongoDb.Views.ViewComponents.DefaultComponents
 {
     public class _DefaultOfferComponentPartial: ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public readonly IOfferService _offerService;
+
+        //Constructor injection yaparak IOfferService'ı kullanabilir hale getiriyoruz. Bu sayede OfferController, IOfferService'ın implementasyonunu kullanarak Offer işlemlerini gerçekleştirebilir.
+        //IOfferService enjekte ediliyor (Dependency Injection)
+        //Controller artık servise erişebiliyor
+        public _DefaultOfferComponentPartial(IOfferService OfferService)
         {
-            return View();
+            _offerService = OfferService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _offerService.GetAllOfferAsync();
+            return View(values);
+
         }
     }
 }
