3bbd69ba87a961d98cd6441b0931222f633b27d8
Melike <22410051025@ogr.erbakan.edu.tr>
2026-04-17 12:02:42 +0300

    feat: Ana sayfa Markalar (Brand) bileseni ve tasarimi olusturuldu

--- DatabseMastery.TransportMongoDb/ViewComponents/DefaultComponents/_DefaultBrandComponentPartial.cs
+++ DatabseMastery.TransportMongoDb/ViewComponents/DefaultComponents/_DefaultBrandComponentPartial.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DatabseMastery.TransportMongoDb.Services.BrandServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DatabseMastery.TransportMongoDb.Views.ViewComponents.DefaultComponents
 {
     public class _DefaultBrandComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public readonly IBrandService _brandService;
+
+        //Constructor injection yaparak IBrandService'ı kullanabilir hale getiriyoruz. Bu sayede BrandController, IBrandService'ın implementasyonunu kullanarak Brand işlemlerini gerçekleştirebilir.
+        //IBrandService enjekte ediliyor (Dependency Injection)
+        //Controller artık servise erişebiliyor
+        public _DefaultBrandComponentPartial(IBrandService brandService)
         {
-            return View();
+            _brandService = brandService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _brandService.GetAllBrandAsync();
+            return View(values);
 
         }
     }
