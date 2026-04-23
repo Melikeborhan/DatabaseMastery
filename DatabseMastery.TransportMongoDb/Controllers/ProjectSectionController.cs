e533d3c0376f4e3a2fcbdf717363b7c36c78df2a
Melike <22410051025@ogr.erbakan.edu.tr>
2026-04-23 14:57:15 +0300

    ProjectSection Controller alanı tamamlandı.

---
+++ DatabseMastery.TransportMongoDb/Controllers/ProjectSectionController.cs
@@ -0,0 +1,56 @@
+﻿using DatabseMastery.TransportMongoDb.Dtos.ProjectSectionDtos;
+using DatabseMastery.TransportMongoDb.Services.ProjectSectionServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatabseMastery.TransportMongoDb.Controllers
+{
+    public class ProjectSectionController : Controller
+    {
+        public readonly IProjectSectionService _projectSectionService;
+
+
+        public ProjectSectionController(IProjectSectionService projectSectionService)
+        {
+            _projectSectionService = projectSectionService;
+        }
+
+        public async Task<IActionResult> ProjectSectionList()
+        {
+            var values = await _projectSectionService.GetAllProjectSectionAsync();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateProjectSection()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProjectSection(CreateProjectSectionDto createProjectSectionDto)
+        {
+            await _projectSectionService.CreateProjectSectionAsync(createProjectSectionDto);
+            return RedirectToAction("ProjectSectionList");
+        }
+
+        public async Task<IActionResult> DeleteProjectSection(string id)
+        {
+            await _projectSectionService.DeleteProjectSectionAsync(id);
+            return RedirectToAction("ProjectSectionList");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateProjectSection(string id)
+        {
+            var values = await _projectSectionService.GetProjectSectionByIdAsync(id);
+            return View(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateProjectSection(UpdateProjectSectionDto updateProjectSectionDto)
+        {
+            await _projectSectionService.UpdateProjectSectionAsync(updateProjectSectionDto);
+            return RedirectToAction("ProjectSectionList");
+        }
+    }
+}
