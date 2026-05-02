7af8cce9d57a1a9db9737ebd74c44942c695c4ca
Melike <22410051025@ogr.erbakan.edu.tr>
2026-05-02 11:49:58 +0300

    Shipment ve ShipmentTracking entities olusturuldu.

---
+++ DatabseMastery.TransportMongoDb/Entities/ShipmentTracking.cs
@@ -0,0 +1,11 @@
+﻿namespace DatabseMastery.TransportMongoDb.Entities
+{
+    public class ShipmentTracking
+    {
+
+        public DateTime EventDate { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public string TrackingStatus { get; set; }
+    }
+}
