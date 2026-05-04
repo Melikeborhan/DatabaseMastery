0fa53410fc843c2ec7c513a6921ba8923c97e961
Melike <22410051025@ogr.erbakan.edu.tr>
2026-05-04 21:07:19 +0300

    ShipmentTracing Service yapıldı.

--- DatabseMastery.TransportMongoDb/Services/ShipmentTrackingServices/IShipmentTrackingService.cs
+++ DatabseMastery.TransportMongoDb/Services/ShipmentTrackingServices/IShipmentTrackingService.cs
@@ -1,6 +1,22 @@
-﻿namespace DatabseMastery.TransportMongoDb.Services.ShipmentTrackingServices
+﻿using DatabseMastery.TransportMongoDb.Dtos.ShipmentTrackingDtos;
+
+namespace DatabseMastery.TransportMongoDb.Services.ShipmentTrackingServices
 {
     public interface IShipmentTrackingService
     {
+        // Belirli kargonun tüm hareketlerini getir
+        Task<List<ResultShipmentTrackingDto>> GetAllTrackingsAsync(string trackingNumber);
+
+        // Yeni hareket ekle
+        Task CreateTrackingAsync(CreateShipmentTrackingDto createDto);
+
+        // Belirli hareketi index ile getir (güncelleme formu için)
+        Task<ResultShipmentTrackingDto> GetTrackingByIndexAsync(string trackingNumber, int index);
+
+        // Hareketi güncelle
+        Task UpdateTrackingAsync(UpdateShipmentTrackingDto updateDto);
+
+        // Hareketi sil
+        Task DeleteTrackingAsync(string trackingNumber, int index);
     }
 }
